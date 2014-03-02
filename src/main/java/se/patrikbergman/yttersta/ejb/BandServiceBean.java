da295f2603691f669f8bdd84113e5ff0baf20053
Patrik Bergman <patrik_bergman@yahoo.com>
2014-03-02 21:18:25 +0100

    added input parameter id

--- src/main/java/se/patrikbergman/yttersta/ejb/BandServiceBean.java
+++ src/main/java/se/patrikbergman/yttersta/ejb/BandServiceBean.java
@@ -5,17 +5,20 @@ import se.patrikbergman.yttersta.entity.BandEntity;
 import javax.ejb.*;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 @Stateless
 @Remote(CrudService.class)
-@TransactionAttribute(TransactionAttributeType.MANDATORY)
 @Path("/band")
 public class BandServiceBean extends CrudServiceBean<BandEntity> {
 
+    public BandServiceBean() {}
+
     @GET
-    @Produces("text/html")
-    public String hello() {
+    @Path("/{id:[0-9][0-9]*}")
+    @Produces("application/json")
+    public String hello(@PathParam("id") int id) {
         return "Hello";
     }
 

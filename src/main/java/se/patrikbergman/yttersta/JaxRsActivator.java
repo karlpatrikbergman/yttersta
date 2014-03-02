b539abe885d4048aaed06931be35875dd9346941
Patrik Bergman <patrik_bergman@yahoo.com>
2014-03-02 22:24:44 +0100

    removed resteasy dependency

--- src/main/java/se/patrikbergman/yttersta/JaxRsActivator.java
+++ src/main/java/se/patrikbergman/yttersta/JaxRsActivator.java
@@ -1,10 +1,18 @@
 package se.patrikbergman.yttersta;
 
+import se.patrikbergman.yttersta.ejb.BandServiceBean;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
 
 @ApplicationPath("/rest")
 public class JaxRsActivator extends Application {
-
+//    public Set<Class<?>> getClasses() {
+//        Set<Class<?>> s = new HashSet<Class<?>>();
+//        s.add(BandServiceBean.class);
+//        return s;
+//    }
 }
 

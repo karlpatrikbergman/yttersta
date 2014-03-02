217886fddb81f3e33aa2ae2c07c4680a8dd20510
Patrik Bergman <patrik_bergman@yahoo.com>
2014-03-02 21:53:35 +0100

    Found problem with "bad argument". Removes unused interface

--- src/main/java/se/patrikbergman/yttersta/ejb/BandServiceBean.java
+++ src/main/java/se/patrikbergman/yttersta/ejb/BandServiceBean.java
@@ -9,7 +9,6 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 @Stateless
-@Remote(CrudService.class)
 @Path("/band")
 public class BandServiceBean extends CrudServiceBean<BandEntity> {
 
